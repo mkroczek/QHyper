f22843225620a3505a15a81709d17ec2c4678f1b
mkroczek <1marcin100@gmail.com>
2024-04-02 20:21:34 +0200

    Add weight calculation for the tree

--- QHyper/problems/algorithms/graph_utils.py
+++ QHyper/problems/algorithms/graph_utils.py
@@ -1,7 +1,21 @@
+import uuid
+from enum import Enum
+
 import networkx as nx
+from anytree import Node
 
 
-# def get_sp_decomposition_tree(graph: nx.DiGraph):
+class Composition(Enum):
+    PARALLEL = 0
+    SERIES = 1
+
+
+class CompositionNode(Node):
+    def __init__(self, left: Node, right: Node, composition: Composition, common_nodes: list[str]):
+        super().__init__(name=uuid.uuid1(), children=[left, right])
+        self.operation = composition
+        self.common_nodes = common_nodes
+
 
 def with_extra_functions(graph: nx.MultiDiGraph):
     def get_sources(self):
@@ -14,27 +28,38 @@ def with_extra_functions(graph: nx.MultiDiGraph):
         if e1[0] != e2[0] or e1[1] != e2[1]:
             raise ValueError("Only identical edges can be reduced")
         u, v = e1[0], e1[1]
+        e1_tree, e2_tree = self.get_edge_tree(e1), self.get_edge_tree(e2)
         self.remove_edges_from([e1, e2])
-        self.add_edge(u, v)
+        self.add_edge(u, v, tree=CompositionNode(e1_tree, e2_tree, Composition.PARALLEL, [u, v]))
 
     def reduce_series(self, e1, e2):
         if e1[1] != e2[0]:
             raise ValueError("Edges must be consecutive to be reduced")
         u, w, v = e1[0], e1[1], e2[1]
+        e1_tree, e2_tree = self.get_edge_tree(e1), self.get_edge_tree(e2)
         self.remove_edges_from([e1, e2])
         self.remove_node(w)
-        self.add_edge(u, v)
+        self.add_edge(u, v, tree=CompositionNode(e1_tree, e2_tree, Composition.SERIES, [w]))
+
+    def get_edge_tree(self, e) -> Node:
+        return self.edges[e].get('tree', Node((e[0], e[1])))
 
     graph.get_sources = get_sources
     graph.get_sinks = get_sinks
     graph.reduce_parallel = reduce_parallel
     graph.reduce_series = reduce_series
+    graph.get_edge_tree = get_edge_tree
     return graph
 
 
 @with_extra_functions
 class GraphDecorator(nx.MultiDiGraph):
     pass
+
+
+def label_edges(graph: nx.MultiDiGraph, label_generator) -> None:
+    for edge in graph.edges(keys=True):
+        graph.edges[edge]['label'] = label_generator(edge)
 
 
 def reduce_parallels(graph: GraphDecorator, node, retrieve_edges):
@@ -49,10 +74,10 @@ def reduce_parallels(graph: GraphDecorator, node, retrieve_edges):
 
 
 def process_vertex(graph: GraphDecorator, node, unsatisfied_nodes: set):
-    reduce_parallels(graph, node, lambda n: list(graph.in_edges(n)))
-    reduce_parallels(graph, node, lambda n: list(graph.out_edges(n)))
+    reduce_parallels(graph, node, lambda n: list(graph.in_edges(n, keys=True)))
+    reduce_parallels(graph, node, lambda n: list(graph.out_edges(n, keys=True)))
     if graph.in_degree(node) == 1 and graph.out_degree(node) == 1:
-        e1, e2 = list(graph.in_edges(node))[0], list(graph.out_edges(node))[0]
+        e1, e2 = list(graph.in_edges(node, keys=True))[0], list(graph.out_edges(node, keys=True))[0]
         graph.reduce_series(e1, e2)
         unsatisfied_nodes.update({e1[0], e2[1]})
 
@@ -61,7 +86,7 @@ def is_trivial_sp_dag(graph: nx.DiGraph):
     return graph.number_of_nodes() == 2 and graph.number_of_edges() == 1
 
 
-def is_sp_dag(graph: nx.DiGraph):
+def recognize_sp(graph: nx.DiGraph) -> GraphDecorator:
     graph = GraphDecorator(graph)
     sources, sinks = graph.get_sources(), graph.get_sinks()
     if len(sources) != 1 or len(sinks) != 1:
@@ -75,4 +100,30 @@ def is_sp_dag(graph: nx.DiGraph):
     while unsatisfied_nodes:
         node = unsatisfied_nodes.pop()
         process_vertex(graph, node, unsatisfied_nodes)
+    return graph
+
+
+def is_sp_dag(graph: nx.DiGraph):
+    graph = recognize_sp(graph)
     return is_trivial_sp_dag(graph)
+
+
+def get_sp_decomposition_tree(graph: nx.DiGraph):
+    graph = recognize_sp(graph)
+    if is_trivial_sp_dag(graph):
+        return graph.get_edge_tree(list(graph.edges(keys=True))[0])
+    else:
+        raise ValueError("Input graph must be SP to build decomposition tree")
+
+
+def apply_weights_on_tree(tree: Node, weights: dict):
+    if 'weight' in tree.__dict__:
+        return
+    if tree.is_leaf:
+        u, v = tree.name
+        tree.weight = weights[u] + weights[v]
+    elif isinstance(tree, CompositionNode):
+        [apply_weights_on_tree(child, weights) for child in tree.children]
+        tree.weight = sum(child.weight for child in tree.children) - sum(weights[n] for n in tree.common_nodes)
+    else:
+        raise TypeError(f"Unable to apply weight to node of type {type(tree)}")
